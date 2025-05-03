a6e1ccc55ee6826191bd60b721a6df1fb1cace6e
dragonhunt02 <jackhunt43@libero.it>
2025-05-03 23:53:46 +0200

    Update enable_ssl.sh

--- scripts/enable_ssl.sh
+++ scripts/enable_ssl.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-cd /var/lib/postgresql/data
+cd /.cockroach-certs
 
-cp ../certs/* .
-chown postgres:postgres server.key
+#cp ../certs/* .
+chown cockroach:cockroach server.key
 chmod 600 server.key
 
-sed -i "s/^#ssl = off/ssl = on/" postgresql.conf
-sed -i "s/^#ssl_ciphers =.*/ssl_ciphers = 'AES256+EECDH:AES256+EDH'/" postgresql.conf
+chown cockroach:cockroach server.crt
+chmod 600 server.crt
+
+#sed -i "s/^#ssl = off/ssl = on/" postgresql.conf
+#sed -i "s/^#ssl_ciphers =.*/ssl_ciphers = 'AES256+EECDH:AES256+EDH'/" postgresql.conf
