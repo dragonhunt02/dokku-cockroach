4c3566c371cbbae26c929a3568331c7c30c0d7e0
dragonhunt02 <jackhunt43@libero.it>
2025-05-04 08:13:11 +0200

    Update enable_ssl.sh

--- scripts/enable_ssl.sh
+++ scripts/enable_ssl.sh
@@ -2,15 +2,29 @@
 
 set -e
 
-mkdir -p /.cockroach-certs
-cd /.cockroach-certs
+CERTS_DIR="/.cockroach-certs"
 
-cp /dokku-cockroach-certs/* .
-chown cockroach:cockroach server.key
-chmod 600 server.key
+mkdir -p $CERTS_DIR
+cd $CERTS_DIR
 
-chown cockroach:cockroach server.crt
-chmod 600 server.crt
+# TODO: Review hostname usage
+if [ ! -f "$CERTS_DIR/ca.crt" ]; then
+    echo "Cockroach certificates not found. Generating new certificates..."
+    cockroach cert create-ca --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
+    cockroach cert create-node localhost $(hostname) --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
+    cockroach cert create-client root --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
+fi
+
+chown -R cockroach:cockroach $CERTS_DIR
+chmod -R 600 $CERTS_DIR
+
+#cp /dokku-cockroach-certs/* .
+
+#chown cockroach:cockroach server.key
+#chmod 600 server.key
+
+#chown cockroach:cockroach server.crt
+#chmod 600 server.crt
 
 #sed -i "s/^#ssl = off/ssl = on/" postgresql.conf
 #sed -i "s/^#ssl_ciphers =.*/ssl_ciphers = 'AES256+EECDH:AES256+EDH'/" postgresql.conf
