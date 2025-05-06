90c553af3f24710825f69ac7fa6cce61899bc6aa
dragonhunt02 <jackhunt43@libero.it>
2025-05-06 14:37:00 +0200

    Update create_certs.sh

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -9,12 +9,13 @@ cd $CERTS_DIR
 
 # TODO: Allow ca.key load from path argument
 # TODO: Review hostname usage
+rm -rf $CERTS_DIR/* || true
 if [ ! -f "$CERTS_DIR/ca.crt" ]; then
     echo "Cockroach CA public key certificate not found. Generating new CA certificates..."
-    rm -rf $CERTS_DIR/* || true
+    #rm -rf $CERTS_DIR/* || true
     cockroach cert create-ca --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
 fi
-
+echo "Generating node certificates..."
 # TODO check if node certs exist
 cockroach cert create-node localhost $(hostname) --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
 cockroach cert create-client root --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
