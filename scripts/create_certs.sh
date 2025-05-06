cca50c868880425e4e400693494026887b15161c
dragonhunt02 <jackhunt43@libero.it>
2025-05-06 07:21:09 +0200

    Create create_certs.sh

---
+++ scripts/create_certs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+CERTS_DIR="/cockroach/certs"
+
+mkdir -p $CERTS_DIR
+cd $CERTS_DIR
+
+# TODO: Allow ca.key load from path argument
+# TODO: Review hostname usage
+if [ ! -f "$CERTS_DIR/ca.crt" ]; then
+    echo "Cockroach CA public key certificate not found. Generating new CA certificates..."
+    rm -rf $CERTS_DIR/* || true
+    cockroach cert create-ca --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
+fi
+
+# TODO check if node certs exist
+cockroach cert create-node localhost $(hostname) --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
+cockroach cert create-client root --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key
+
+
+chown -R cockroach:cockroach $CERTS_DIR
+chmod -R 600 $CERTS_DIR
