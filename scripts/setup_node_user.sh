8ab65a209f349d8d51d79dcaf6ad326b76621350
dragonhunt02 <jackhunt43@libero.it>
2025-05-06 16:09:02 +0200

    Update setup_node_user.sh

--- scripts/setup_node_user.sh
+++ scripts/setup_node_user.sh
@@ -5,14 +5,14 @@ set -e
 CERTS_DIR="/cockroach/certs"
 
 user_already_exists() {
-   $cockroach sql --certs-dir=$CERTS_DIR --host=127.0.0.1 --execute "select username FROM [SHOW ROLES]" \
+   cockroach sql --certs-dir=$CERTS_DIR --host=127.0.0.1 --execute "select username FROM [SHOW ROLES]" \
      | tail -n +2 \
      | grep "^${1}\$"
 }
 
 create_default_user() {
   COCKROACH_USER="dokku"
-  cockroach="/usr/local/bin/cockroach"
+  cockroa="/usr/local/bin/cockroach"
   #$1"
   echo >&2 "Create user: $COCKROACH_USER"
   if [[ -z $COCKROACH_USER ]]; then
@@ -20,16 +20,16 @@ create_default_user() {
   fi
 
   if [[ -z $(user_already_exists "$COCKROACH_USER") ]]; then
-    $cockroach cert create-client --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key "$COCKROACH_USER"
+    cockroach cert create-client --certs-dir=${CERTS_DIR} --ca-key=${CERTS_DIR}/ca.key "$COCKROACH_USER"
 
     local user_query="CREATE USER "$COCKROACH_USER""
     if [[ -n "$COCKROACH_PASSWORD" ]]; then
       user_query+=" WITH PASSWORD '$COCKROACH_PASSWORD'"
     fi
-    $cockroach sql --certs-dir=$CERTS_DIR --host=127.0.0.1 --execute "$user_query;"
+    cockroach sql --certs-dir=$CERTS_DIR --host=127.0.0.1 --execute "$user_query;"
   
-    $cockroach sql --certs-dir=$CERTS_DIR --host=127.0.0.1 --execute "GRANT ALL ON DATABASE "$COCKROACH_DATABASE" TO "$COCKROACH_USER";"
-    $cockroach sql --certs-dir=$CERTS_DIR --host=127.0.0.1 --execute "GRANT admin TO "$COCKROACH_USER";"
+    cockroach sql --certs-dir=$CERTS_DIR --host=127.0.0.1 --execute "GRANT ALL ON DATABASE "$COCKROACH_DATABASE" TO "$COCKROACH_USER";"
+    cockroach sql --certs-dir=$CERTS_DIR --host=127.0.0.1 --execute "GRANT admin TO "$COCKROACH_USER";"
 
     echo >&2 "finished creating default user \"$COCKROACH_USER\""
   else
